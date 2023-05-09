3c6b2081f1412687ff82acd599c692314f38cad3
Ben Borszcz <ben.borszcz@gmail.com>
2023-05-09 15:17:34 -0400

    organized elo/slight change

--- tracking.py
+++ tracking.py
@@ -34,49 +34,61 @@ class Tracking:
 
 
     def update_data(self, game):
-        team1 = game.team1
-        team2 = game.team2
+        # Update the games played and wins for the winner and loser
+        winner, loser = self.update_elo(game)
 
-        winner = team1
-        loser = team2
-
-        if game.score[0] < game.score[1]:
-            winner = team2
-            loser = team1
-          
         winner.add_game()
         winner.add_win()
         loser.add_game()
 
-        winner_elo = (winner.player1.elo + winner.player2.elo) / 2
-        loser_elo = (loser.player1.elo + loser.player2.elo) / 2
-        rating_diff = loser_elo - winner_elo
+    def update_elo(self, game):
+        # Get the playing teams
+        team1 = game.team1
+        team2 = game.team2
 
+        # Determine the winner and loser based on the game score
+        winner = team1
+        loser = team2
+        if game.score[0] < game.score[1]:
+            winner = team2
+            loser = team1
+
+        # Calculate the Elo rating difference for the teams
+        team_elo_difference = loser.elo - winner.elo
+
+        # Set the base win reward
         win_reward = 8
 
+        # Set multipliers
         rating_diff_multiplier = 0.05
-
-        compensated_win_reward = win_reward + (rating_diff * rating_diff_multiplier)
-
-        point_diff = game.point_difference()
-
         point_diff_multiplier = 0.125
 
+        # Calculate the point difference between the winner and the loser
+        point_diff = game.point_difference()
+
+        # Calculate the compensated win reward based on the rating difference
+        compensated_win_reward = win_reward + (team_elo_difference * rating_diff_multiplier)
+        
+        # Calculate the rating change based on the compensated win reward and point difference
         rating_change = round((compensated_win_reward + (point_diff * point_diff_multiplier)), 2)
 
-        for winning_player in [winner.player1, winner.player2]:
-            winning_player.update_elo(round(rating_change+0.5, 2))
-            
-        for losing_player in [loser.player1, loser.player2]:
-            losing_player.update_elo(round(-rating_change+0.5, 2))
+        # Update the Elo ratings for the teams and the players
+        self.update_team_elo(winner, loser, rating_change)
+        self.update_player_elo(winner, loser, rating_change)
 
-        # Team Elo
-        
-        team_elo_difference = loser.elo - winner.elo
-        team_compensated_win_reward = win_reward + (team_elo_difference * rating_diff_multiplier)
-        team_rating_change = round((team_compensated_win_reward + (point_diff * point_diff_multiplier)), 2)
-        winner.update_elo(round(team_rating_change+0.5, 2))
-        loser.update_elo(round(-team_rating_change+0.5, 2))
+        return winner, loser
+
+    def update_team_elo(self, winner, loser, rating_change):
+        # Update the team Elo ratings for the winner and the loser
+        winner.update_elo(round(rating_change + 0.5, 2))
+        loser.update_elo(round(-rating_change + 0.5, 2))
+
+    def update_player_elo(self, winner, loser, rating_change):
+        # Update the individual Elo ratings for the winner and loser players
+        for winning_player in [winner.player1, winner.player2]:
+            winning_player.update_elo(round(rating_change + 0.5, 2))
+        for losing_player in [loser.player1, loser.player2]:
+            losing_player.update_elo(round(-rating_change + 0.5, 2))
         
 
     def get_player(self, name):
